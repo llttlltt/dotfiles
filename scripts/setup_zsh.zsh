571b6f6f8d2684a9802d185dc0fe5b22ca80f76e
Elliott-Liu <16389800+Elliott-Liu@users.noreply.github.com>
2022-07-22 12:25:12 +0100

    Add conditional checking if /etc/shells exists
    This prevents the script from asking for sudo access unnecessarily.

--- scripts/setup_zsh.zsh
+++ scripts/setup_zsh.zsh
@@ -5,8 +5,13 @@ echo "\n<<< STARTING ZSH SETUP >>>"
 # Installation unnecessary; it's in the brewfile.
 
 ZSH_PATH=/opt/homebrew/bin/zsh
+
+if grep ${ZSH_PATH} /etc/shells; then
+  echo "${ZSH_PATH} already exists in /etc/shells, skipping step..."
+else
 echo "Enter superuser (sudo) password to edit /etc/shells"
-echo '/opt/homebrew/bin/zsh' | sudo tee -a '/etc/shells' > /dev/null
+  echo $ZSH_PATH | sudo tee -a '/etc/shells' > /dev/null
+fi
 
 echo "Enter user password to change login shell"
 chsh -s "/opt/homebrew/bin/zsh"
