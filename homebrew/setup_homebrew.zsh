26260b89dd8287ed45e49707028eb016d788229e
Elliott-Liu <16389800+Elliott-Liu@users.noreply.github.com>
2022-07-22 01:00:34 +0100

    Update comments and describe what is happening

--- homebrew/setup_homebrew.zsh
+++ homebrew/setup_homebrew.zsh
@@ -1,13 +1,15 @@
 #!/usr/bin/env zsh
 
-echo "\n<<< Starting Homebrew Setup >>>\n"
+echo "<<< STARTING HOMEBREW INSTALL >>>"
 
 if exists brew; then
-  echo "Homebrew already exists, skipping install."
+  echo "Homebrew already exists, skipping install.\n"
 else
-  echo "Homebrew doesn't exist, continuing with install."
+  echo "Homebrew doesn't exist, continuing with install.\n"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
+
+echo "<<< BUNDLING HOMEBREW >>>\n"
 
 # TODO: Keep an eye out for a different `--no-quarantine` solution.
 # Currently, you can't do `brew bundle --no-quarantine` as an option.
