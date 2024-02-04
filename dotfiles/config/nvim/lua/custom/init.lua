5f56b8dac73c22c6a93913bf05ae6acfbad8ecb0
Elliott-Liu <16389800+Elliott-Liu@users.noreply.github.com>
2024-02-04 20:16:07 +0000

    Update nvim init.lua config

--- dotfiles/config/nvim/lua/custom/init.lua
+++ dotfiles/config/nvim/lua/custom/init.lua
@@ -1,5 +1,20 @@
 require "custom.commands"
 require "custom.autocmds"
 
+vim.opt.termguicolors = true
 vim.opt.title = true
 vim.g.dap_virtual_text = true
+
+vim.cmd("aunmenu PopUp.How-to\\ disable\\ mouse")
+vim.cmd("aunmenu PopUp.-1-")
+
+-- Copy
+vim.api.nvim_set_keymap('n', '<YourEscapeSequenceForCmdC>', '"+y', {
+    noremap = true,
+    silent = true
+})
+-- Paste
+vim.api.nvim_set_keymap('n', '<YourEscapeSequenceForCmdV>', '"+p', {
+    noremap = true,
+    silent = true
+})
