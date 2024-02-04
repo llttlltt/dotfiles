bbd4d9f64f9708b44235f3d2af5a0be1a37ef9bb
Elliott-Liu <16389800+Elliott-Liu@users.noreply.github.com>
2024-02-04 20:41:44 +0000

    Probably just going to yank

--- dotfiles/config/nvim/lua/custom/init.lua
+++ dotfiles/config/nvim/lua/custom/init.lua
@@ -7,14 +7,3 @@ vim.g.dap_virtual_text = true
 
 vim.cmd("aunmenu PopUp.How-to\\ disable\\ mouse")
 vim.cmd("aunmenu PopUp.-1-")
-
--- Copy
-vim.api.nvim_set_keymap('n', '<YourEscapeSequenceForCmdC>', '"+y', {
-    noremap = true,
-    silent = true
-})
--- Paste
-vim.api.nvim_set_keymap('n', '<YourEscapeSequenceForCmdV>', '"+p', {
-    noremap = true,
-    silent = true
-})
