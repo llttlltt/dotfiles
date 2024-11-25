b1cf98415ae618b8b4de18cd32b4914bfe57f74a
Elliott-Liu <16389800+Elliott-Liu@users.noreply.github.com>
2024-11-25 16:57:19 +0800

    Ensure taplo (TOML) installed

--- dotfiles/config/nvim/lua/custom/plugins/lsp.lua
+++ dotfiles/config/nvim/lua/custom/plugins/lsp.lua
@@ -219,6 +219,7 @@ return {
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'taplo', -- Used to format TOML files
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
