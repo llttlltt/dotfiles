d20423157811303d70b591ba403a91f133d70c3b
Elliott-Liu <16389800+Elliott-Liu@users.noreply.github.com>
2025-10-23 12:06:00 +0800

    Update required packages

--- scripts/setup_node.zsh
+++ scripts/setup_node.zsh
@@ -4,44 +4,34 @@ echo "📦 Starting Node.js setup..."
 
 # Function to check and install NPM packages
 check_and_install_npm_package() {
-  local package=$1
-  if npm list --global "$package" >/dev/null 2>&1; then
-    echo "✅ $package already installed, skipping..."
-  else
-    echo "🔧 Installing $package..."
-    npm install --global "$package"
-  fi
+    local package=$1
+    if npm list --global "$package" >/dev/null 2>&1; then
+        echo "✅ $package already installed, skipping..."
+    else
+        echo "🔧 Installing $package..."
+        npm install --global "$package"
+    fi
 }
 
-# Array of NPM packages to install
+# Global NPM package array
 npm_packages=(
-  "firebase-tools"
-  "@angular/cli"
-  "@ionic/cli"
-  "typescript"
-  "json-server"
-  "http-server"
-  "trash-cli"
-  "nodemon"
-  "corepack"
-  "postman-to-openapi"
 )
 
 # Check if Node.js is installed
 if command -v node >/dev/null 2>&1; then
-  echo "✅ Node $(node --version) & NPM $(npm --version) already installed, skipping Node installation..."
+    echo "✅ Node $(node --version) & NPM $(npm --version) already installed, skipping Node installation..."
 else
-  echo "🔧 Installing Node & NPM with nvm..."
-  echo "🔧 Installing LTS version..."
-  nvm install --lts
-  echo "🔧 Installing latest version..."
-  nvm install node --default
+    echo "🔧 Installing Node & NPM with nvm..."
+    echo "🔧 Installing LTS version..."
+    nvm install --lts
+    echo "🔧 Installing latest version..."
+    nvm install node --default
 fi
 echo ""
 
 echo "🔧 Installing global NPM packages...\n"
 for package in "${npm_packages[@]}"; do
-  check_and_install_npm_package "$package"
+    check_and_install_npm_package "$package"
 done
 echo "\n✅ Global NPM packages installation complete."
 echo ""
