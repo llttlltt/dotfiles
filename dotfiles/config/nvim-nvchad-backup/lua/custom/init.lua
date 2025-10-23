3ac3e82ab364f258c8bbb9b41efb734a758ca6ca
Elliott-Liu <16389800+Elliott-Liu@users.noreply.github.com>
2025-10-23 11:21:25 +0800

    Remove nvim config

--- dotfiles/config/nvim-nvchad-backup/lua/custom/init.lua
+++
@@ -1,9 +0,0 @@
-require "custom.commands"
-require "custom.autocmds"
-
-vim.opt.termguicolors = true
-vim.opt.title = true
-vim.g.dap_virtual_text = true
-
-vim.cmd("aunmenu PopUp.How-to\\ disable\\ mouse")
-vim.cmd("aunmenu PopUp.-1-")
