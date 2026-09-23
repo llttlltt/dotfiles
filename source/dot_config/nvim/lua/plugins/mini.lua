51e19204047c26eb4ce3686aecff975914c79c24
Elliott-Liu <16389800+Elliott-Liu@users.noreply.github.com>
2026-09-23 14:36:49 +0800

    feat(nvim): add syntax-aware function text objects

--- source/dot_config/nvim/lua/plugins/mini.lua
+++ source/dot_config/nvim/lua/plugins/mini.lua
@@ -1,12 +1,22 @@
 return {
 	"nvim-mini/mini.ai", -- Collection of various small independent plugins/modules
-	opts = {
-		-- Better Around/Inside textobjects
-		--
-		-- Examples:
-		--  - va)  - [V]isually select [A]round [)]paren
-		--  - yinq - [Y]ank [I]nside [N]ext [Q]uote
-		--  - ci'  - [C]hange [I]nside [']quote
-		n_lines = 500,
-	},
+	dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
+	opts = function()
+		return {
+			-- Better Around/Inside textobjects
+			--
+			-- Examples:
+			--  - va)  - [V]isually select [A]round [)]paren
+			--  - yinq - [Y]ank [I]nside [N]ext [Q]uote
+			--  - ci'  - [C]hange [I]nside [']quote
+			n_lines = 500,
+			custom_textobjects = {
+				-- Function definitions: yaf/yif, daf/dif, vaf/vif.
+				f = require("mini.ai").gen_spec.treesitter({
+					a = "@function.outer",
+					i = "@function.inner",
+				}),
+			},
+		}
+	end,
 }
